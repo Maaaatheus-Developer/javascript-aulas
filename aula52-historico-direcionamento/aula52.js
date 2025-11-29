35103ac411e71233b2b7e65c03751aa36fb63923
Matheus Minetto <matheusminetto140502@gmail.com>
2025-11-29 17:46:09 -0300

    feat: adicionar formulário e lógica de redirecionamento na aula 52; criar nova aula 53 com botões de diálogo

--- aula52-historico-direcionamento/aula52.js
+++ aula52-historico-direcionamento/aula52.js
@@ -1,6 +1,13 @@
 const url = document.getElementById("url");
-const btn_url = document.getElementById("btn-url");
+const btn_url = document.getElementById("btn_url");
 
-btn_url.addEventListener("click", () => {
-    window.location = 'google.com';
+btn_url.addEventListener("click", (evt) => {
+  // window.location.replace("https://www.google.com.br"); Redireciona e remova a url do histórico
+  //window.location.assign("https://www.google.com.br"); // Redireciona e mantém a url no histórico, não deleta
+  //window.location.reload();
+  // window.history.back(); // Volta para a página anterior no histórico
+  //window.history.forward(); // Avança para a próxima página no histórico
+  // window.history.go(); // Vai para uma página específica no histórico, 0 é a página atual, -1 é a anterior, 1 é a próxima
+  console.log(url.value);
+  window.location = url.value
 });
